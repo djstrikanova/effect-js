1fbf98f6ea7ea2aaa828f8863d466cf24d8f441b
David Britt <david@effect.ai>
2021-11-12 16:45:21 +0100

    rollup 🔫

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,46 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+// import typescript from 'rollup-plugin-typescript2';
+// import { resolve } from 'path/posix';
+import resolve from 'rollup-plugin-node-resolve'
+// import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import json from '@rollup/plugin-json';
+import auto from '@rollup/plugin-auto-install'
+import commonjs from 'rollup-plugin-commonjs';
+import * as bn from 'bn.js'
+import replace from 'rollup-plugin-replace'
+
+// import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
   output: {
-    name: 'bundle.umd.js',
-    dir: 'dist',
-    format: 'umd' // umd, es, iife, cjs, amd, system
+    file: 'dist/index.umd.js',
+    // dir: 'dist',
+    name: 'effectSdk',
+    format: 'umd', // umd, es, iife, cjs, amd, system
+    sourcemap: true,
+
   },
-  plugins: [typescript()]
+   
+  plugins: [
+    resolve({  browser: true  }),
+    typescript({ }),
+    json({  }),
+    sourceMaps({  }),
+    replace({ 'process.env.NODE_DEBUG': false }),
+    commonjs({ 
+      namedExports: {
+        'node_modules/@ethersproject/bignumber/node_modules/bn.js/lib/bn.js': ['bn'],
+
+      }
+    }),
+    // commonjs({ include: [
+    //   './node_modules/**',
+    //   './src/**',
+    // ] }),
+    // auto(),
+
+  ]
 };
